a6f1ef7bc63980156f817410d7f1ae96bfd0d151
Winds <asfuyao@gmail.com>
2019-02-14 15:50:20 +0800

    添加windows和office的KMS激活脚本

---
+++ Windows/KMS客户端激活windows_10_enterprise_ltsc_2019.cmd
@@ -0,0 +1,5 @@
+@rem 使用管理员分别执行下面命令可激活系统
+@rem 适用版本：Windows 10 企业版 2019 长期服务
+slmgr /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+slmgr /skms bestanswer.f3322.net
+slmgr /ato
---
+++ Windows/KMS服务激活Office2016.cmd
@@ -0,0 +1,8 @@
+@echo off
+c:
+cd "C:\Program Files\Microsoft Office\Office16"
+cscript ospp.vbs /sethst:bestanswer.f3322.net
+cscript ospp.vbs /act
+
+
+pause
