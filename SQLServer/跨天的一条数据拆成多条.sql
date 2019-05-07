a5268c1130e6d2c869cdb289de9dfa1ff7de63dc
Winds <asfuyao@gmail.com>
2019-05-07 10:45:46 +0800

    Update 跨天的一条数据拆成多条.sql
    纠正开始和结束为一天计算错误的情况

--- SQLServer/跨天的一条数据拆成多条.sql
+++ SQLServer/跨天的一条数据拆成多条.sql
@@ -1,8 +1,16 @@
+--开始时间
+DECLARE @beginDT DATETIME = '2019-05-07 08:58:00.000';
+--结束时间
+DECLARE @endDT DATETIME = '2019-05-07 09:08:00.000';
+
 SELECT t.d,
        CASE
-         WHEN d = CONVERT(DATE, @beginDT) THEN DATEDIFF(MINUTE, @beginDT, DATEADD(DAY, 1, d))
+         WHEN d = CONVERT(DATE, @beginDT) THEN CASE
+                                                 WHEN d = CONVERT(DATE, @endDT) THEN DATEDIFF(MINUTE, @beginDT, @endDT)
+                                               ELSE DATEDIFF(MINUTE, @beginDT, DATEADD(DAY, 1, d))
+                                               END
          WHEN d > CONVERT(DATE, @beginDT)
-          AND d < CONVERT(DATE, @endDT) THEN 24*60
+          AND d < CONVERT(DATE, @endDT) THEN 24 * 60
          WHEN d = CONVERT(DATE, @endDT) THEN
            DATEDIFF(MINUTE, CONVERT(DATETIME, CONVERT(NVARCHAR(10), d, 120) + ' 00:00:00'), @endDT)
        ELSE 0
@@ -10,4 +18,4 @@ SELECT t.d,
 FROM   ( SELECT CONVERT(NVARCHAR(10), DATEADD(DAY, number, @beginDT), 120) d
          FROM   master..spt_values
          WHERE  type = 'p'
-           AND  number <= DATEDIFF(DAY, @beginDT, @endDT) ) t;
+           AND  number <= DATEDIFF(DAY, @beginDT, @endDT)) t;
